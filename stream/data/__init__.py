f8ae1a1d64e70a60a11adef22ff4d7d42021c4fa
simondelarue <sim.delarue@gmail.com>
2022-02-04 12:28:14 +0100

    removed sub projects (will be included in other repos)

--- stream/data/__init__.py
+++
@@ -1,5 +0,0 @@
-''' Data module '''
-
-from stream.data.load import load_data
-from stream.data.parse import convert_edge_list, from_edge_list
-from stream.data.preprocessing import split
--- stream/ranking/__init__.py
+++
@@ -1,5 +0,0 @@
-''' Ranking module '''
-
-from stream.ranking.base import BaseRanking
-from stream.ranking.pagerank import PageRank
-from stream.ranking.postprocess import top_k, MRR
